cee62baf4ba525a7a8c24ec01ded6edbe66ad381
Gokul <gokulchandan24@gmail.com>
2025-08-17 16:51:33 +0530

    a

---
+++ backend/bookmarks/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    is_premium = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='bookmarks')
+    url = models.URLField(max_length=200)
+    title = models.CharField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.url
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
