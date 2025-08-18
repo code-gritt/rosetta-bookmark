518accda48cacec224ae105d085203af1b5efc86
Gokul <gokulchandan24@gmail.com>
2025-08-18 17:49:05 +0530

    a

--- backend/bookmark_manager/urls.py
+++ backend/bookmark_manager/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def redirect_frontend(request):
+    return redirect("https://rosetta-bookmark.vercel.app")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('bookmarks.urls')),
+    path("", redirect_frontend),
+    path("admin/", admin.site.urls),
+    path("api/", include("bookmarks.urls")),
 ]
