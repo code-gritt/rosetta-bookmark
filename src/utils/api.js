846f009dc13636ba4b1889742ce768be25c77b12
Gokul <gokulchandan24@gmail.com>
2025-08-19 18:34:57 +0530

    a

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,8 +4,7 @@ import useAuthStore from "../store/useAuthStore";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://127.0.0.1:8000/api/" ||
-    "https://rosetta-bookmark-2.onrender.com/api",
+    "https://rosetta-bookmark-2.onrender.com/api/",
 });
 
 // Attach token automatically
